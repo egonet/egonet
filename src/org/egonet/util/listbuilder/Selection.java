823f7173757a98b368bce3134b27f77cef345871
Sona Muthuvijayan <sona_m@bebr.ufl.edu>
2008-04-14 17:26:04 +0000

    Working on adding "Text" questions to graph

--- src/org/egonet/util/listbuilder/Selection.java
+++ src/org/egonet/util/listbuilder/Selection.java
@@ -1,71 +1,73 @@
 package org.egonet.util.listbuilder;
 
-public class Selection
-{
+public class Selection {
 	private String string;
+
 	private int index;
-	private int value; 
+
+	private int value;
+
 	private boolean adjacent;
-	
-	public Selection()
-	{
+
+	public Selection() {
 		this.string = "";
 		this.value = -1;
 		this.index = -1;
 		this.adjacent = false;
 	}
-	
-	public Selection(String string, int value, int index, boolean adjacent)
-	{
+
+	public Selection(String string, int value, int index, boolean adjacent) {
 		this.string = string;
 		this.value = value;
 		this.index = index;
 		this.adjacent = adjacent;
 	}
-	
-	public String toString()
-	{
-		//return "[Selection \"" + string + "\", index = "+index+", value = "+value+", adjacent = "+adjacent+"]";
+
+	public String toString() {
+		// return "[Selection \"" + string + "\", index = "+index+", value =
+		// "+value+", adjacent = "+adjacent+"]";
 		return string;
 	}
 
-	public boolean isAdjacent()
-	{
+	public boolean isAdjacent() {
 		return adjacent;
 	}
 
-	public void setAdjacent(boolean adjacent)
-	{
+	public void setAdjacent(boolean adjacent) {
 		this.adjacent = adjacent;
 	}
 
-	public int getIndex()
-	{
+	public int getIndex() {
 		return index;
 	}
 
-	public void setIndex(int index)
-	{
+	public void setIndex(int index) {
 		this.index = index;
 	}
 
-	public String getString()
-	{
+	public String getString() {
 		return string;
 	}
 
-	public void setString(String string)
-	{
+	public void setString(String string) {
 		this.string = string;
 	}
 
-	public int getValue()
-	{
+	public int getValue() {
 		return value;
 	}
 
-	public void setValue(int value)
-	{
+	public void setValue(int value) {
 		this.value = value;
+	}
+
+	public boolean equals(Selection selection) {
+		if (this.adjacent == selection.adjacent
+				&& this.index == selection.index
+				&& this.string.equals(selection.string)
+				&& this.value == selection.value) {
+			return true;
+		}
+		return false;
 	}
 }
