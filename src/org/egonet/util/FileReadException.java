98088d6a0bfe77f88bc498da916317a28bce0e8a
Martin Smith <martin@mbs3.org>
2008-10-25 23:02:14 +0000

    Collapsing package structure further, removing some duplicate classes like 'Shared', and also adding the useful exception constructors to the application-specific exception classes.

---
+++ src/org/egonet/util/FileReadException.java
@@ -0,0 +1,50 @@
+/***
+ * Copyright (c) 2008, Endless Loop Software, Inc.
+ * 
+ * This file is part of EgoNet.
+ * 
+ * EgoNet is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EgoNet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.egonet.util;
+
+/**
+ * @author admin
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class FileReadException extends Exception
+{
+
+	public FileReadException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileReadException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileReadException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileReadException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/org/egonet/util/FileWriteException.java
@@ -0,0 +1,39 @@
+/***
+ * Copyright (c) 2008, Endless Loop Software, Inc.
+ * 
+ * This file is part of EgoNet.
+ * 
+ * EgoNet is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EgoNet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.egonet.util;
+
+public class FileWriteException extends Exception
+{
+
+	public FileWriteException() {
+		super();
+	}
+
+	public FileWriteException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FileWriteException(String message) {
+		super(message);
+	}
+
+	public FileWriteException(Throwable cause) {
+		super(cause);
+	}
+}
